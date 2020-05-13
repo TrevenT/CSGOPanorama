7772f195198282668eb717e56d2c2cf445c3436e
TrevenT <28998668+TrevenT@users.noreply.github.com>
2020-05-13 20:35:06 +1000

    16 July 2019 - 23:25:47 UTC

--- panorama/panorama/scripts/mainmenu_news.js
+++ panorama/panorama/scripts/mainmenu_news.js
@@ -39,21 +39,9 @@ var NewsPanel = (function () {
 		} );
 	};
 
-	var _OnSteamIsPlaying = function()
-	{
-		$.GetContextPanel().SetHasClass( 'news-panel-style-short-entires', EmbeddedStreamAPI.IsVideoPlaying() );
-	};
-
-	var _ResetNewsEntryStyle = function()
-	{
-		$.GetContextPanel().RemoveClass( 'news-panel-style-short-entires' );
-	};
-
 	return {
 		GetRssFeed			: _GetRssFeed,
 		OnRssFeedReceived: _OnRssFeedReceived,
-		OnSteamIsPlaying: _OnSteamIsPlaying,
-		ResetNewsEntryStyle: _ResetNewsEntryStyle
 	};
 })();
 
@@ -61,6 +49,4 @@ var NewsPanel = (function () {
 (function () {
 	NewsPanel.GetRssFeed();
 	$.RegisterForUnhandledEvent( "PanoramaComponent_Blog_RSSFeedReceived", NewsPanel.OnRssFeedReceived );
-	$.RegisterForUnhandledEvent( "PanoramaComponent_EmbeddedStream_VideoPlaying", NewsPanel.OnSteamIsPlaying );
-	$.RegisterForUnhandledEvent( "StreamPanelClosed", NewsPanel.ResetNewsEntryStyle );
 })();
